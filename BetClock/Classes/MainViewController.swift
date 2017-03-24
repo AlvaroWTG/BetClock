d232d5a3aa3594307257f98b0437c79e2839b338
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2017-03-24 15:10:52 +0100

    Initial commit

---
+++ BetClock/Classes/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  BetClock
+//
+//  Created by WebToGo on 3/24/17.
+//  Copyright © 2017 Alvaro GMH. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
