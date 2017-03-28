e9df7c03757191cdd36fa7b876667c5a9d6b1ec9
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2017-03-28 12:31:08 +0200

    - Upcoming. First steps configuring the local push notifications

--- BetClock/Classes/AppDelegate.swift
+++ BetClock/Classes/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 struct Colors { // Constants for color definitions used in the app
     static let Color147855 = UIColor.init(red: 20/255, green: 120/255, blue: 85/255, alpha: 1)
@@ -21,7 +22,7 @@ struct Size { // Constants for screen size used in the app
 }
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     /** Property that represents the window of the app */
     var window: UIWindow?
@@ -33,6 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
         UITabBar.appearance().tintColor = Colors.Color147855
         UINavigationBar.appearance().tintColor = UIColor.white
+        requestNotificationsAuthorization()
 
         return true
     }
@@ -59,6 +61,56 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    //MARK: Inherited functions from UILocalNotification delegate
 
+    func application(_ application: UIApplication, didReceive notification: UILocalNotification) {
+        if application.applicationState == .active {
+            UIAlertView.init(title: "BetClock", message: notification.alertBody, delegate: nil, cancelButtonTitle: "OK").show()
+        }
+        application.applicationIconBadgeNumber = 0;
+    }
+
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        switch response.actionIdentifier { // Determine the user action
+        case UNNotificationDismissActionIdentifier:
+            print("Dismiss Action")
+        case UNNotificationDefaultActionIdentifier:
+            print("Default")
+        case "Snooze":
+            print("Snooze")
+        case "Delete":
+            print("Delete")
+        default:
+            print("Unknown action")
+        }
+        completionHandler()
+    }
+
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert,.sound]) // Play sound and show alert to the user
+    }
+
+    //MARK: Auxiliary functions
+
+    func requestNotificationsAuthorization() {
+        if #available(iOS 10.0, *) {
+            let center = UNUserNotificationCenter.current()
+            let options: UNAuthorizationOptions = [.alert, .sound]
+            center.delegate = self
+            center.requestAuthorization(options: options) {
+                (granted, error) in
+                if !granted {
+                    NSLog("[UNNotification] Error! Something went wrong. Error 500 - %@", error?.localizedDescription ?? "unknown")
+                }
+            }
+            center.getNotificationSettings { (settings) in
+                if settings.authorizationStatus != .authorized { // Notifications not allowed
+                    NSLog("[UNNotification] Log: Notifications are not currently allowed")
+                }
+            }
+        }
+    }
 }
 
